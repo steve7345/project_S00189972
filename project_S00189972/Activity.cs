4e5d41c69f0f41339bcdd7676a2fd42fc2e6548c
s00189972 <s00189972@mail.itsligo.ie>
2019-11-21 13:08:46 +0000

    project

---
+++ project_S00189972/Activity.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_S00189972 
+{
+    public enum ActivityType { Land, Water, Air}
+    public class Activity : IComparable
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime ActivityDate { get; set; }
+        public decimal Cost { get; set; }
+        public ActivityType TypeOfActivity { get; internal set; }
+
+        public Activity(string name, string description, DateTime activityDate, decimal cost)
+        {
+            Name = name;
+            Description = Description;
+            ActivityDate = activityDate;
+            Cost = cost;
+            
+            
+        }
+
+        public Activity()
+        {
+        }
+        public override string ToString()
+        {
+            return $"{Name} - { ActivityDate.ToShortDateString()}";
+        }
+
+        public int CompareTo(object obj)
+        {
+            Activity that = (Activity)obj;
+            return ActivityDate.CompareTo(that.ActivityDate);
+        }
+    }
+
+
+   
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- project_S00189972/MainWindow.xaml.cs
+++ project_S00189972/MainWindow.xaml.cs
@@ -20,9 +20,120 @@ namespace project_S00189972
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Activity> activities = new List<Activity>();
+        Activity activityDescription;
         public MainWindow()
         {
             InitializeComponent();
+        }
+        
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+
+            Activity l1 = new Activity()
+            {
+                Name = "Treking",
+                Description = "Instructor led group trek through local mountains.",
+                ActivityDate = new DateTime(2019, 06, 01),
+                TypeOfActivity = ActivityType.Land,
+                Cost = 20m
+
+            };
+
+            Activity l2 = new Activity()
+            {
+                Name = "Mountain Biking",
+                Description = "Instructor led half day mountain biking.  All equipment provided.",
+                ActivityDate = new DateTime(2019, 06, 02),
+                TypeOfActivity = ActivityType.Water,
+                Cost = 30m
+            };
+
+            Activity l3 = new Activity()
+            {
+                Name = "Abseiling",
+                Description = "Experience the rush of adrenaline as you descend cliff faces from 10-500m.",
+                ActivityDate = new DateTime(2019, 06, 03),
+                TypeOfActivity = ActivityType.Land,
+                Cost = 40m
+            };
+
+            Activity w1 = new Activity()
+            {
+                Name = "Kayaking",
+                Description = "Half day lakeland kayak with island picnic.",
+                ActivityDate = new DateTime(2019, 06, 01),
+                TypeOfActivity = ActivityType.Water,
+                Cost = 40m
+            };
+
+            Activity w2 = new Activity()
+            {
+                Name = "Surfing",
+                Description = "2 hour surf lesson on the wild atlantic way",
+                ActivityDate = new DateTime(2019, 06, 02),
+                TypeOfActivity = ActivityType.Water,
+                Cost = 25m
+            };
+
+            Activity w3 = new Activity()
+            {
+                Name = "Sailing",
+                Description = "Full day lakeland kayak with island picnic.",
+                ActivityDate = new DateTime(2019, 06, 03),
+                TypeOfActivity = ActivityType.Water,
+                Cost = 50m
+            };
+
+            Activity a1 = new Activity()
+            {
+                Name = "Parachuting",
+                Description = "Experience the thrill of free fall while you tandem jump from an airplane.",
+                ActivityDate = new DateTime(2019, 06, 01),
+                TypeOfActivity = ActivityType.Air,
+                Cost = 100m
+            };
+
+            Activity a2 = new Activity()
+            {
+                Name = "Hang Gliding",
+                Description = "Soar on hot air currents and enjoy spectacular views of the coastal region.",
+                ActivityDate = new DateTime(2019, 06, 02),
+                TypeOfActivity = ActivityType.Air,
+                Cost = 80m
+            };
+
+            Activity a3 = new Activity()
+            {
+                Name = "Helicopter Tour",
+                Description = "Experience the ultimate in aerial sight-seeing as you tour the area in our modern helicopters",
+                ActivityDate = new DateTime(2019, 06, 03),
+                TypeOfActivity = ActivityType.Air,
+                Cost = 200m
+            };
+
+            activities.Add(l1);
+            activities.Add(l2);
+            activities.Add(l3);
+            activities.Add(w1);
+            activities.Add(w2);
+            activities.Add(w3);
+            activities.Add(a1);
+            activities.Add(a2);
+            activities.Add(a3);
+
+            lbxActivities.ItemsSource = activities;
+        }
+
+        private void LbxActivities_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //Activity activity = lbxActivities.SelectedItem as Activity;
+
+            //activityDescription = (Activity)TextBlock;
+
+            TblkShowDespription.Text = activityDescription.Description;
         }
     }
 }
