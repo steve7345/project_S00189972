469b46962b82c3debd759728b9ea61bb6c2b18a1
s00189972 <s00189972@mail.itsligo.ie>
2019-12-03 16:02:34 +0000

    finished

--- project_S00189972/Activity.cs
+++ project_S00189972/Activity.cs
@@ -6,33 +6,38 @@ using System.Threading.Tasks;
 
 namespace project_S00189972 
 {
+    //enum for activity type 
     public enum ActivityType { Land, Water, Air}
     public class Activity : IComparable
     {
+        //properties
         public string Name { get; set; }
         public string Description { get; set; }
         public DateTime ActivityDate { get; set; }
         public decimal Cost { get; set; }
         public ActivityType TypeOfActivity { get; internal set; }
 
+        // construstors
         public Activity(string name, string description, DateTime activityDate, decimal cost)
         {
             Name = name;
             Description = Description;
             ActivityDate = activityDate;
             Cost = cost;
-            
-            
         }
 
         public Activity()
         {
         }
+
+        //to diplaye the name and activity date in the listboxes
         public override string ToString()
         {
             return $"{Name} - { ActivityDate.ToShortDateString()}";
         }
 
+
+        //icomparablle to sort dates
         public int CompareTo(object obj)
         {
             Activity that = (Activity)obj;
--- project_S00189972/MainWindow.xaml.cs
+++ project_S00189972/MainWindow.xaml.cs
@@ -21,17 +21,21 @@ namespace project_S00189972
     public partial class MainWindow : Window
     {
         List<Activity> activities = new List<Activity>();
-        Activity activityDescription;
+        List<Activity> selectedactivities = new List<Activity>();
+        List<Activity> filteredactivities = new List<Activity>();
+        decimal totalCost = 0;
+
         public MainWindow()
         {
             InitializeComponent();
         }
-        
+
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            
 
+
+            //information for each activity
             Activity l1 = new Activity()
             {
                 Name = "Treking",
@@ -114,6 +118,7 @@ namespace project_S00189972
                 Cost = 200m
             };
 
+            //add activities to the listbox
             activities.Add(l1);
             activities.Add(l2);
             activities.Add(l3);
@@ -123,17 +128,161 @@ namespace project_S00189972
             activities.Add(a1);
             activities.Add(a2);
             activities.Add(a3);
-
+            activities.Sort();
             lbxActivities.ItemsSource = activities;
         }
 
+
+        //method to show the description when activity is selected
         private void LbxActivities_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            //Activity activity = lbxActivities.SelectedItem as Activity;
 
-            //activityDescription = (Activity)TextBlock;
+            // show description in text box when activity is clicked
+            Activity activity = lbxActivities.SelectedItem as Activity;
+            if (activity != null)
+            {
 
-            TblkShowDespription.Text = activityDescription.Description;
+                TblkShowDespription.Text = activity.Description;
+            }
+
         }
+
+
+
+        //Add activity to cart from activity listbox
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            //figure out what is been selected
+            Activity selectedActivity = lbxActivities.SelectedItem as Activity;
+
+            //null check
+            if (selectedActivity != null)
+            {
+                Boolean dateConflict = false;
+                foreach (Activity activity in selectedactivities)
+                {
+
+                    if (activity.ActivityDate == selectedActivity.ActivityDate)
+                    {
+                        dateConflict = true;
+                        MessageBox.Show("Date already selected");
+                    }
+                }
+                if (!dateConflict)
+                {
+
+                    //move activity from left listbox to right
+                    activities.Remove(selectedActivity);
+                    selectedactivities.Add(selectedActivity);
+                    
+                    totalCost += selectedActivity.Cost;
+                    TblkTotalCost.Text = totalCost.ToString();
+                    RefreshScreen();
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Nothing selected");
+            }
+            activities.Sort();
+        }
+
+
+        //move activity back from cart to activity list box
+        private void BtnRemove_Click(object sender, RoutedEventArgs e)
+        {
+            //figure out what is been selected
+            Activity selectedActivity = lbxCart.SelectedItem as Activity;
+
+            //null check
+            if (selectedActivity != null)
+            {
+                //move activity from left listbox to right
+
+                activities.Add(selectedActivity);
+                selectedactivities.Remove(selectedActivity);
+                
+                totalCost -= selectedActivity.Cost;
+                TblkTotalCost.Text = totalCost.ToString();
+                
+                RefreshScreen();
+            }
+            activities.Sort();
+        }
+
+        //method handles all radio buttons 
+        private void RbAll_Click(object sender, RoutedEventArgs e)
+        {
+            //empties list
+            filteredactivities.Clear();
+
+            if (rbAll.IsChecked == true)
+            {
+                //show all activities
+                RefreshScreen();
+
+            }
+            else if (rbLand.IsChecked == true)
+            {
+                //show all land activities
+                foreach (Activity activity in activities)
+                {
+                    if (activity.TypeOfActivity == ActivityType.Land)
+                    {
+                        filteredactivities.Add(activity);
+                        RefreshActivities();
+                    }
+                }
+
+
+            }
+            else if (rbWater.IsChecked == true)
+            {
+                //show all water activities
+                foreach (Activity activity in activities)
+                {
+                    if (activity.TypeOfActivity == ActivityType.Water)
+                    {
+                        filteredactivities.Add(activity);
+                        RefreshActivities();
+                    }
+                }
+            }
+            else if (rbAir.IsChecked == true)
+            {
+                //show all air activities
+                foreach (Activity activity in activities)
+                {
+                    if (activity.TypeOfActivity == ActivityType.Air)
+                    {
+                        filteredactivities.Add(activity);
+                        RefreshActivities();
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select option for land, air, water or all");
+            }
+        }
+
+        //method to refresh the activities for radio buttons
+        private void RefreshActivities()
+        {
+            lbxActivities.ItemsSource = null;
+            lbxActivities.ItemsSource = filteredactivities;
+        }
+
+        //method to refresh screen
+        private void RefreshScreen()
+        {
+            lbxActivities.ItemsSource = null;
+            lbxActivities.ItemsSource = activities;
+
+            lbxCart.ItemsSource = null;
+            lbxCart.ItemsSource = selectedactivities;
+        }
+
     }
 }
